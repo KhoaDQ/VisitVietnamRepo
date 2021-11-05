576134599d3bf0fded510bb1d5e5624492f99f5c
Khoa Dang <76057587+KhoaDQ@users.noreply.github.com>
2021-11-05 23:07:25 +0700

    footer and init homepage

--- First Prototype/SourceVisitVietnam/reacttut/my-app/src/Component/Navigation/Navigation.js
+++ First Prototype/SourceVisitVietnam/reacttut/my-app/src/Component/Navigation/Navigation.js
@@ -1,64 +1,99 @@
-import React,{Component} from 'react';
-import {NavLink} from 'react-router-dom';
-import {Navbar,Nav} from 'react-bootstrap'; 
-import { Form, Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
-import './Navigation.css';
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
+import {
+  Form,
+  Button,
+  FormGroup,
+  FormControl,
+  ControlLabel,
+} from "react-bootstrap";
+import "./Navigation.css";
 
-export class Navigation extends Component{
+export class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      strSearch: "",
+    };
 
-    constructor(props){
-        super(props);
-        this.state={
-            strSearch:""
-        };
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-        this.handleSearch= this.handleSearch.bind(this);
-        this.handleChange= this.handleChange.bind(this);
-    }
+  handleSearch() {
+    console.log(this.state.strSearch);
+    this.props.handleSearchContent(this.state.strSearch);
+  }
 
-    handleSearch(){
-        console.log(this.state.strSearch);
-        this.props.handleSearchContent(this.state.strSearch);
-    }
+  handleChange(event) {
+    this.setState({ strSearch: event.target.value });
+  }
 
-    handleChange(event){
-        this.setState({strSearch: event.target.value});
-    }
+  render() {
+    return (
+      <div className="Navigation">
+        <Navbar bg="white" className="Navigation-Navbar pt-0 pb-0">
+          <Navbar.Collapse id="basic-navbar-nav" className="route-button">
+            <Nav>
+              <NavLink
+                className="d-inline px-5 bg-white text-dark text-uppercase font-weight-bold btn"
+                to="/"
+              >
+                Home
+              </NavLink>
+              <NavLink
+                className="d-inline px-3 bg-white text-dark text-uppercase font-weight-bold btn"
+                to="/event"
+              >
+                Event
+              </NavLink>
+              <NavLink
+                className="d-inline px-3 bg-white text-dark text-uppercase font-weight-bold btn"
+                to="/foody"
+              >
+                Foody
+              </NavLink>
+              <NavLink
+                className="d-inline px-3 bg-white text-dark text-uppercase font-weight-bold btn"
+                to="/homestay"
+              >
+                HomeStay
+              </NavLink>
+              <NavLink
+                className="d-inline px-3 bg-white text-dark text-uppercase font-weight-bold btn"
+                to="/clothes"
+              >
+                Clothes
+              </NavLink>
+              <NavLink
+                className="d-inline px-3 bg-white text-dark text-uppercase font-weight-bold btn"
+                to="/gift"
+              >
+                Gift
+              </NavLink>
+            </Nav>
+          </Navbar.Collapse>
 
-    render(){
-        return(
-            <div className="Navigation">
-            <Navbar bg="white" className="Navigation-Navbar pt-0 pb-0">
-                <Navbar.Collapse id="basic-navbar-nav" className="route-button">
-                    <Nav>
-                        <NavLink className="d-inline px-5 bg-white text-dark text-uppercase font-weight-bold btn" to="/">
-                            Home
-                        </NavLink>
-                        <NavLink className="d-inline px-3 bg-white text-dark text-uppercase font-weight-bold btn" to="/event">
-                            Event
-                        </NavLink>
-                        <NavLink className="d-inline px-3 bg-white text-dark text-uppercase font-weight-bold btn" to="/foody">
-                            Foody
-                        </NavLink>
-                        <NavLink className="d-inline px-3 bg-white text-dark text-uppercase font-weight-bold btn" to="/homestay">
-                            HomeStay
-                        </NavLink>
-                        <NavLink className="d-inline px-3 bg-white text-dark text-uppercase font-weight-bold btn" to="/clothes">
-                            Clothes
-                        </NavLink>
-                        <NavLink className="d-inline px-3 bg-white text-dark text-uppercase font-weight-bold btn" to="/gift">
-                            Gift
-                        </NavLink>
-                    </Nav>
-                </Navbar.Collapse>
-
-                <Form inline className="Navigation-Search">
-                    <FormControl value={this.state.strSearch} type="text" placeholder="Search" ref="search" className="Form p-0 m-0" onChange={this.handleChange}/>
-                    <NavLink onClick={this.handleSearch} className=" border-img-full btn p-2 m-0" to="/search">GO</NavLink>
-                </Form>
-            </Navbar>
-            </div>
-        )
-        
-    }
+          <Form inline className="Navigation-Search">
+            <FormControl
+              value={this.state.strSearch}
+              type="text"
+              placeholder="Search"
+              ref="search"
+              className="Form p-0 m-0"
+              onChange={this.handleChange}
+            />
+            <NavLink
+              onClick={this.handleSearch}
+              className=" border-img-full btn p-2 m-0"
+              to="/search"
+            >
+              GO
+            </NavLink>
+          </Form>
+        </Navbar>
+      </div>
+    );
+  }
 }
--- Second Prototype/VisitVietnam-mern/client/src/home/Home.js
+++ Second Prototype/VisitVietnam-mern/client/src/home/Home.js
@@ -1,7 +1,33 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import "../assets/css/base.css";
+import "./Home.css";
 
 export class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    return <div>This is HomePage</div>;
+    return (
+      <div className="Container Home">
+        <div id="slider" className="Banner">
+          <div className="text-content">
+            <div className="text-heading">Home</div>
+            <div className="text-desc">
+              We provide the most important information and useful guidance to
+              make your trip to Vietnam comfortable and smooth. I'm sure we're
+              giving you what you really need
+            </div>
+          </div>
+        </div>
+
+        <div className="Container-background pt-5 pb-5">
+          <div className="Container__Content">This is Content of Home Page</div>
+        </div>
+
+        <div className="clear"></div>
+      </div>
+    );
   }
 }
